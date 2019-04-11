4f2389865d8121d08540b22077aa55f9924570b5
liuxun <liuxun030792>
2019-04-11 16:42:04 +0800

    内存模型

--- src/com/liuxun/Main.java
+++ src/com/liuxun/Main.java
@@ -3,6 +3,8 @@ package com.liuxun;
 public class Main {
 
     public static void main(String[] args) {
+        String test = "123123";
+        System.out.println("---:"+test.intern());
         System.out.println("Hello World!");
     }
 }
